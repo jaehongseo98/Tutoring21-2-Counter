ca2640b603c8d85569f2b5983f8139702e816235
lejehwan <rdsa257@naver.com>
2021-09-28 19:31:18 +0900

    asdf

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let minus = document.querySelector('.minus')
+let plus = document.querySelector('.plus')
+let count = document.querySelector('.count')
+let cnt = 0
+
+minus.addEventListener('click', () =>{
+    cnt --
+    count.value = cnt
+})
+
+plus.addEventListener('click', () => {
+    cnt++
+    count.value = cnt
+})
